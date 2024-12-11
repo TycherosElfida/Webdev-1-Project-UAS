c45904de443551de33526e21fd4e9fe22b540b08
Tycheros Elfida <sanderskeanedylan@gmail.com>
2024-12-11 08:06:41 +0700

    why-choose-us lebih di bagusin bagian responsivenya

--- includes/sections/why-choose-us.php
+++ includes/sections/why-choose-us.php
@@ -1,4 +1,4 @@
-<section class="why-choose-us m-5">
+<section class="why-choose-us m-4">
     <div class="container">
         <div class="row">
             <div class="col text-center mb-4">
@@ -6,31 +6,31 @@
             </div>
         </div>
         <div class="row align-items-center justify-content-center gap-4 text-center">
-            <div class="col-6 col-md-4 col-lg-2 border rounded p-3 shadow">
+            <div class="col-7 col-md-4 col-lg-2 border rounded p-3 shadow">
                 <div class="image-container">
                     <img src="assets/images/why-choose-us/professional-installation-outdoor-camera.jpg" alt="Professional Installation" class="img-fluid mb-2 rounded-circle">
                 </div>
                 <h5>Professional Installation</h5>
             </div>
-            <div class="col-6 col-md-4 col-lg-2 border rounded p-3 shadow">
+            <div class="col-7 col-md-4 col-lg-2 border rounded p-3 shadow">
                 <div class="image-container">
                     <img src="assets/images/why-choose-us/installation-warranty_1.jpg" alt="Installation and Service Warranty" class="img-fluid mb-2 rounded-circle">
                 </div>
                 <h5>Installation and Service Warranty</h5>
             </div>
-            <div class="col-6 col-md-4 col-lg-2 border rounded p-3 shadow">
+            <div class="col-7 col-md-4 col-lg-2 border rounded p-3 shadow">
                 <div class="image-container">
                     <img src="assets/images/why-choose-us/Experienced in Diverse Environments.webp" alt="Experienced in Diverse Environments" class="img-fluid mb-2 rounded-circle">
                 </div>
                 <h5>Experienced in Diverse Environments</h5>
             </div>
-            <div class="col-6 col-md-4 col-lg-2 border rounded p-3 shadow">
+            <div class="col-7 col-md-4 col-lg-2 border rounded p-3 shadow">
                 <div class="image-container">
                     <img src="assets/images/why-choose-us/Trusted by Leading Clients.jpeg" alt="Trusted by Leading Clients" class="img-fluid mb-2 rounded-circle">
                 </div>
                 <h5>Trusted by Leading Clients</h5>
             </div>
-            <div class="col-6 col-md-4 col-lg-2 border rounded p-3 shadow">
+            <div class="col-7 col-md-4 col-lg-2 border rounded p-3 shadow">
                 <div class="image-container">
                     <img src="assets/images/why-choose-us/Customer Support & Satisfaction.png" alt="Customer Support & Satisfaction" class="img-fluid mb-2 rounded-circle">
                 </div>
